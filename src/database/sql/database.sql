79fcc9510c974a80df787d7d014b07aab4fb99e2
Carmelo Naim <carmelonaim30@gmail.com>
2023-12-22 19:59:58 -0400

    der logica de la bdd tablas funciones triggers y consultas actualizadas

--- src/database/sql/database.sql
+++ src/database/sql/database.sql
@@ -15,7 +15,7 @@ CREATE DOMAIN dom_volume numeric(6,2) CHECK (VALUE > 0);
 
 CREATE TYPE type_role AS ENUM ('admin', 'client');
 CREATE TYPE type_currency AS ENUM ('USD', 'VES');
-CREATE TYPE type_payment_method AS ENUM ('efectivo', 'transferencia', 'tarjeta', 'pago movil');
+CREATE TYPE type_payment_method AS ENUM ('efectivo', 'transferencia', 'tarjeta', 'pago movil', 'saldo a favor');
 CREATE TYPE type_unit_measure AS ENUM ('kg', 'g', 'l', 'ml', 'unidades');
 
 -- Tables
@@ -45,7 +45,6 @@ CREATE TABLE clients (
   residence_address dom_residence_address DEFAULT NULL,
   phone_number dom_phone_number DEFAULT NULL,
   email dom_email,
-  password dom_password,
   role type_role NOT NULL,
   created_at dom_created_at,
   CONSTRAINT clients_pk PRIMARY KEY (user_id),
@@ -77,6 +76,7 @@ CREATE TABLE admins (
 -- 4
 CREATE TABLE products (
   product_id INTEGER GENERATED ALWAYS AS IDENTITY,
+  serial_number dom_reference DEFAULT NULL,
   name dom_name NOT NULL,
   unit_measure type_unit_measure NOT NULL,
   volume dom_volume NOT NULL,
@@ -87,15 +87,57 @@ CREATE TABLE products (
   updated_at dom_created_at,
   CONSTRAINT products_pk PRIMARY KEY (product_id),
   CONSTRAINT products_unique_name UNIQUE (name),
+  CONSTRAINT products_unique_serial_number UNIQUE (serial_number),
+  CONSTRAINT check_sale_price CHECK (sale_price > 0),-- esto funciona o intrfiere el dom_amount? que si puede ser 0
   CONSTRAINT check_available_units CHECK (available_units >= 0)
 );
 -- 5
-CREATE TABLE payments (
+CREATE TABLE purchases (
+  client_id INTEGER,
+  purchase_id INTEGER GENERATED ALWAYS AS IDENTITY,
+  date dom_created_at NOT NULL,
+  total_purchase_amount numeric(8,2) NOT NULL DEFAULT 0,
+  its_paid BOOLEAN NOT NULL DEFAULT FALSE,
+  seller_id INTEGER NOT NULL,
+  CONSTRAINT purchases_pk PRIMARY KEY (client_id, purchase_id),
+  CONSTRAINT purchases_fk FOREIGN KEY (client_id)
+   REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+  CONSTRAINT purchases_fk_seller FOREIGN KEY (seller_id)
+   REFERENCES admins (user_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
+-- 6
+CREATE TABLE purchase_details (
+  client_id INTEGER,
+  purchase_id INTEGER,
+  product_id INTEGER,
+  loaded_units INTEGER NOT NULL,
+  charget_price dom_amount NOT NULL DEFAULT 0, -- porque el default 0?
+  total_item_amount dom_amount NOT NULL GENERATED ALWAYS AS (
+    charget_price * loaded_units
+  ) STORED,
+  CONSTRAINT purchase_details_pk PRIMARY KEY (client_id, purchase_id, product_id),
+  CONSTRAINT purchase_details_fk_purchase FOREIGN KEY (client_id, purchase_id)
+   REFERENCES purchases (client_id, purchase_id)
+    ON UPDATE CASCADE 
+    ON DELETE RESTRICT,
+  CONSTRAINT purchase_details_fk_product FOREIGN KEY (product_id)
+   REFERENCES products (product_id)
+    ON UPDATE CASCADE 
+    ON DELETE CASCADE
+);
+-- 7
+CREATE TABLE payments (-- sospechoso esta permitiendo null en el fk client_id
+  client_id INTEGER,
+  purchase_id INTEGER,
   payment_id INTEGER GENERATED ALWAYS AS IDENTITY,
   date dom_created_at,
   amount dom_amount NOT NULL,
   currency type_currency NOT NULL,
-  reference dom_reference NOT NULL,
+  reference dom_reference NOT NULL,-- necesito que no se repita pero que en caso de que sea efectivo no sea obligatorio o que se pueda repetir
   exchange_rate numeric(5,2) NOT NULL,
   payment_method type_payment_method NOT NULL,
   amount_in_usd dom_amount GENERATED ALWAYS AS (
@@ -105,72 +147,26 @@ CREATE TABLE payments (
     END
   ) STORED,
   created_at dom_created_at,
-  CONSTRAINT payments_pk PRIMARY KEY (payment_id),
-  CONSTRAINT check_amount CHECK (amount > 0),
-  CONSTRAINT check_exchange_rate CHECK (exchange_rate > 0)
+  CONSTRAINT payments_pk PRIMARY KEY (client_id, purchase_id, payment_id),
+  CONSTRAINT payments_fk FOREIGN KEY (client_id, purchase_id)
+   REFERENCES purchases (client_id, purchase_id)
+    ON UPDATE CASCADE 
+    ON DELETE RESTRICT,
+  CONSTRAINT check_amount CHECK (amount > 0),-- esto funciona o intrfiere el dom_amount? que si puede ser 0
+  CONSTRAINT check_exchange_rate CHECK (exchange_rate > 0),
+  CONSTRAINT uq_reference UNIQUE (payment_id, reference)
 );
 
--- 6
-CREATE TABLE purchases (
-  client_id INTEGER,
-  purchase_id INTEGER GENERATED ALWAYS AS IDENTITY,
-  date dom_created_at NOT NULL,
-  total_purchase_amount dom_amount NOT NULL DEFAULT 0,
-  its_paid BOOLEAN NOT NULL DEFAULT FALSE,
-  seller_id INTEGER NOT NULL,
-  CONSTRAINT purchases_pk PRIMARY KEY (client_id, purchase_id),
-  CONSTRAINT purchases_fk FOREIGN KEY (client_id)
-   REFERENCES users (user_id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-  CONSTRAINT purchases_fk_seller FOREIGN KEY (seller_id)
-   REFERENCES users (user_id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
--- 7
-CREATE TABLE purchase_details (
-  client_id INTEGER,
-  purchase_id INTEGER,
-  product_id INTEGER,
-  loaded_units INTEGER NOT NULL,
-  charget_price dom_amount NOT NULL DEFAULT 0,
-  total_item_amount dom_amount NOT NULL DEFAULT 0,
-  CONSTRAINT purchase_details_pk PRIMARY KEY (client_id, purchase_id, product_id),
-  CONSTRAINT purchase_details_fk_purchase FOREIGN KEY (client_id, purchase_id)
-   REFERENCES purchases (client_id, purchase_id)
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT,
-  CONSTRAINT purchase_details_fk_product FOREIGN KEY (product_id)
-   REFERENCES products (product_id)
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT
-);
 -- 8
-CREATE TABLE correspond (
-  payment_id INTEGER,
-  client_id INTEGER,
-  purchase_id INTEGER,
-  amount_paid dom_amount NOT NULL DEFAULT 0,
-  CONSTRAINT correspond_pk PRIMARY KEY (payment_id, client_id, purchase_id),
-  CONSTRAINT correspond_fk_payment FOREIGN KEY (payment_id)
-   REFERENCES payments (payment_id)
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT,
-  CONSTRAINT correspond_fk_purchase FOREIGN KEY (client_id, purchase_id)
-   REFERENCES purchases (client_id, purchase_id)
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT
-);
--- 9
+
 CREATE TABLE buy (
   admin_id INTEGER,
-  product_id INTEGER,
   buy_idx INTEGER GENERATED ALWAYS AS IDENTITY,
+  product_id INTEGER,
   units INTEGER NOT NULL,
   date dom_created_at,
   price dom_amount NOT NULL,
-  CONSTRAINT buy_pk PRIMARY KEY (admin_id, product_id, buy_idx),
+  CONSTRAINT buy_pk PRIMARY KEY (admin_id, buy_idx),
   CONSTRAINT buy_fk_admin FOREIGN KEY (admin_id)
    REFERENCES admins (user_id)
     ON UPDATE CASCADE 
@@ -184,7 +180,7 @@ CREATE TABLE buy (
 -- Functions
 
 -- function to update updated_at column
-CREATE FUNCTION update_updated_at ()
+CREATE OR REPLACE FUNCTION update_updated_at ()
 RETURNS TRIGGER AS $$
 BEGIN
   NEW.updated_at = CURRENT_TIMESTAMP;
@@ -207,30 +203,49 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- function to update available_units column
-CREATE OR REPLACE FUNCTION update_available_units()
- RETURNS TRIGGER AS $$
-BEGIN
-  UPDATE products
-  SET available_units = available_units + NEW.units
-  WHERE product_id = NEW.product_id;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- function to update available_units column - ON PURCHASE_DETAILS TABLE
-CREATE OR REPLACE FUNCTION update_available_units_at_products()
+-- function to update available_units column on products from buy table
+CREATE OR REPLACE FUNCTION update_available_units_from_buy() -- tambien tengo que considerar cuando actualizo en buy
  RETURNS TRIGGER AS $$
  DECLARE
   aux_units INTEGER;
 BEGIN
-  SELECT available_units FROM products WHERE product_id = NEW.product_id INTO aux_units;
-  IF aux_units < NEW.loaded_units THEN
-    RAISE EXCEPTION 'No hay suficientes unidades disponibles del producto %', NEW.product_id;
-  ELSIF aux_units >= NEW.loaded_units THEN
-    UPDATE products
-    SET available_units = available_units - NEW.loaded_units
+  IF TG_OP = 'DELETE' THEN
+    SELECT available_units FROM products WHERE product_id = OLD.product_id INTO aux_units;
+    IF aux_units < OLD.units THEN
+      RAISE EXCEPTION 'No hay suficientes unidades disponibles del producto % quiza ya han sido vendidas', OLD.product_id;
+    END IF;
+    UPDATE products 
+    SET available_units = available_units - OLD.units 
+    WHERE product_id = OLD.product_id;
+  ELSIF TG_OP = 'INSERT' THEN
+    UPDATE products 
+    SET available_units = available_units + NEW.units 
     WHERE product_id = NEW.product_id;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- function to update available_units column on products - FROM PURCHASE_DETAILS TABLE
+CREATE OR REPLACE FUNCTION update_available_units_from_purchase_details()
+ RETURNS TRIGGER AS $$
+ DECLARE
+  aux_units INTEGER;
+  loaded_units_option INTEGER;
+BEGIN
+  IF TG_OP = 'INSERT' THEN
+    SELECT available_units FROM products WHERE product_id = NEW.product_id INTO aux_units;
+    IF aux_units < NEW.loaded_units THEN
+      RAISE EXCEPTION 'No hay suficientes unidades disponibles del producto %', NEW.product_id;
+    ELSIF aux_units >= NEW.loaded_units THEN
+      UPDATE products
+      SET available_units = available_units - NEW.loaded_units
+      WHERE product_id = NEW.product_id;
+    END IF;
+  ELSIF TG_OP = 'DELETE' THEN
+    UPDATE products
+    SET available_units = available_units + OLD.loaded_units
+    WHERE product_id = OLD.product_id;
   END IF;
   RETURN NEW;
 END;
@@ -244,80 +259,8 @@ CREATE OR REPLACE FUNCTION charge_sale_price()
 BEGIN
   SELECT sale_price FROM products WHERE product_id = NEW.product_id INTO aux_sale_price;
   UPDATE purchase_details
-  SET charget_price = aux_sale_price, total_item_amount = aux_sale_price * NEW.loaded_units
+  SET charget_price = aux_sale_price
   WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id AND product_id = NEW.product_id;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- function to calculate rest of payment
-CREATE OR REPLACE FUNCTION calculate_rest_of_payment(par_client_id INTEGER, par_purchase_id INTEGER)
- RETURNS dom_amount AS $$
- DECLARE
-  rest_of_payment dom_amount;
-BEGIN
-  SELECT  p.amount_in_usd - sum(c.amount_paid) as rest FROM payments p INNER JOIN correspond c ON p.payment_id = c.payment_id WHERE c.client_id = par_client_id AND c.purchase_id = par_purchase_id  GROUP BY(p.amount_in_usd ) INTO rest_of_payment;
-  RETURN rest_of_payment;
-END;
-$$ LANGUAGE plpgsql;
-
--- function to calculate total paid purchase
-CREATE OR REPLACE FUNCTION calculate_total_paid_purchase(par_client_id INTEGER, par_purchase_id INTEGER)
- RETURNS dom_amount AS $$
- DECLARE
-  total_purchase_paid dom_amount;
-BEGIN
-  SELECT SUM(amount_paid) FROM correspond WHERE client_id = par_client_id AND purchase_id = par_purchase_id INTO total_purchase_paid;
-  RETURN total_purchase_paid;
-END;
-$$ LANGUAGE plpgsql;
-
--- function to calculate total debt purchase per item
-CREATE OR REPLACE FUNCTION calculate_total_debt_purchase(par_client_id INTEGER, par_purchase_id INTEGER)
- RETURNS dom_amount AS $$
- DECLARE
-  aux_total_purchase_amount dom_amount;
-BEGIN
-  SELECT total_purchase_amount FROM purchases WHERE client_id = par_client_id AND purchase_id = par_purchase_id INTO aux_total_purchase_amount;
-  RETURN aux_total_purchase_amount;
-END;
-$$ LANGUAGE plpgsql;
-
--- function to update amount_paid column - ON CORRESPOND TABLE
-CREATE OR REPLACE FUNCTION update_amount_paid_on_correspond()
- RETURNS TRIGGER AS $$
- DECLARE
- rest_of_payment dom_amount;
- total_purchase_amount dom_amount;
- total_purchase_paid dom_amount;
-BEGIN
-  rest_of_payment = calculate_rest_of_payment(NEW.client_id, NEW.purchase_id);
-  total_purchase_amount = calculate_total_debt_purchase(NEW.client_id, NEW.purchase_id);
-  total_purchase_paid = calculate_total_paid_purchase(NEW.client_id, NEW.purchase_id);
-
-  IF rest_of_payment = 0 THEN
-    RAISE EXCEPTION 'No queda saldo disponible del pago %', NEW.payment_id;
-  END IF;
-  
-  IF (total_purchase_paid < total_purchase_amount) and (rest_of_payment > (total_purchase_amount - total_purchase_paid)) THEN
-    UPDATE correspond
-    SET amount_paid = amount_paid + (total_purchase_amount - total_purchase_paid)
-    WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id AND payment_id = NEW.payment_id;
-  END IF;
-
-IF rest_of_payment <= (total_purchase_amount - total_purchase_paid) THEN
-    UPDATE correspond
-    SET amount_paid = amount_paid + rest_of_payment
-    WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id AND payment_id = NEW.payment_id;
-END IF;
-
- total_purchase_paid = calculate_total_paid_purchase(NEW.client_id, NEW.purchase_id);
- 
-  IF total_purchase_paid = total_purchase_amount THEN
-    UPDATE purchases
-    SET its_paid = TRUE
-    WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id;
-  END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -326,23 +269,68 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION update_total_purchase_amount()
  RETURNS TRIGGER AS $$
  DECLARE
+  purchase_id_option INTEGER;
+  client_id_option INTEGER;
   aux_total_purchase_amount dom_amount;
 BEGIN
+  client_id_option = NEW.client_id;
+  purchase_id_option = NEW.purchase_id;
+  IF TG_OP = 'DELETE' THEN
+    purchase_id_option = OLD.purchase_id;
+    client_id_option = OLD.client_id;
+  END IF;
+  
   SELECT sum(total_item_amount) 
     FROM purchase_details 
-    WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id INTO aux_total_purchase_amount;
+    WHERE client_id = client_id_option AND purchase_id = purchase_id_option INTO aux_total_purchase_amount;
+  IF aux_total_purchase_amount IS NULL THEN
+    aux_total_purchase_amount = 0;
+  END IF;
   UPDATE purchases
     SET total_purchase_amount = aux_total_purchase_amount
-    WHERE client_id = NEW.client_id AND purchase_id = NEW.purchase_id;
+    WHERE client_id = client_id_option AND purchase_id = purchase_id_option;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+-- fuction to update its_paid column on purchases table from payments table
+CREATE OR REPLACE FUNCTION update_its_paid()
+ RETURNS TRIGGER AS $$
+ DECLARE
+  aux_total_purchase_amount dom_amount;
+  aux_rest numeric(8,2);
+  aux_its_paid BOOLEAN DEFAULT TRUE;
+  client_id_option INTEGER;
+  purchase_id_option INTEGER;
+BEGIN
+  client_id_option = NEW.client_id;
+  purchase_id_option = NEW.purchase_id;
+  IF TG_OP = 'DELETE' THEN
+    client_id_option = OLD.client_id;
+    purchase_id_option = OLD.purchase_id;
+  END IF;
+
+  SELECT total_purchase_amount FROM purchases WHERE client_id = client_id_option AND purchase_id = purchase_id_option INTO aux_total_purchase_amount;
+  SELECT sum(p.amount_in_usd) - aux_total_purchase_amount FROM payments p WHERE p.client_id = client_id_option AND p.purchase_id = purchase_id_option INTO aux_rest;
+  IF aux_rest IS NULL THEN
+    aux_rest = total_purchase_amount*-1;
+  END IF;
+  IF aux_rest < 0 THEN
+    aux_its_paid = FALSE;
+  ELSIF aux_rest >= 0 THEN
+    aux_its_paid = TRUE;
+  END IF;
+    UPDATE purchases
+    SET its_paid = aux_its_paid
+    WHERE client_id = client_id_option AND purchase_id = purchase_id_option;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Triggers
 -- trigger to update total_purchase_amount column - ON PURCHASES TABLE
 CREATE TRIGGER update_total_purchase_amount
-AFTER INSERT ON purchase_details
+AFTER INSERT OR DELETE ON purchase_details
 FOR EACH ROW
 EXECUTE FUNCTION update_total_purchase_amount();
 
@@ -354,9 +342,9 @@ EXECUTE FUNCTION insert_client_or_admin();
 
 -- trigger to update available_units column - ON BUY TABLE
 CREATE TRIGGER update_available_units
-AFTER INSERT ON buy
+AFTER INSERT OR DELETE ON buy
 FOR EACH ROW
-EXECUTE FUNCTION update_available_units();
+EXECUTE FUNCTION update_available_units_from_buy();
 
 -- trigger to update updated_at column - ON PRODUCTS TABLE
 CREATE TRIGGER update_updated_at_products
@@ -365,10 +353,10 @@ FOR EACH ROW
 EXECUTE FUNCTION update_updated_at();
 
 -- trigger to update available_units column - ON PURCHASE_DETAILS TABLE
-CREATE TRIGGER update_available_units_at_purchase_details
-AFTER INSERT ON purchase_details
+CREATE TRIGGER update_available_units_at_products
+AFTER INSERT OR DELETE ON purchase_details
 FOR EACH ROW
-EXECUTE FUNCTION update_available_units_at_products();
+EXECUTE FUNCTION update_available_units_from_purchase_details();
 
 -- trigger to charge sale_price on purchase_details table
 CREATE TRIGGER charge_sale_price
@@ -376,9 +364,10 @@ AFTER INSERT ON purchase_details
 FOR EACH ROW
 EXECUTE FUNCTION charge_sale_price();
 
--- trigger to updadte amount_paid column - ON CORRESPOND TABLE
-CREATE OR REPLACE TRIGGER update_amount_paid_on_correspond
-AFTER INSERT ON correspond
+-- trigger to update its_paid column on purchases table from payments table
+CREATE TRIGGER update_its_paid
+AFTER INSERT OR DELETE ON payments
 FOR EACH ROW
-EXECUTE FUNCTION update_amount_paid_on_correspond();
+EXECUTE FUNCTION update_its_paid();
+
 COMMIT;
